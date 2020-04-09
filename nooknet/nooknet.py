4b14ca9d407ff6283e21bd7778b7bcc2f03c58d1
GitAcrown <Acrown@outlook.fr>
2020-04-09 20:02:33 +0200

    Correction sur le reset du preload

--- nooknet/nooknet.py
+++ nooknet/nooknet.py
@@ -270,9 +270,10 @@ class Nooknet:
 
         La date doit être au format JJ/MM/AAAA/PP avec pour PP soit AM (matin) ou PM (après-midi)
         Ex: 01/04/2020/PM"""
-        if not date: date = datetime.now().strftime("%d/%m/%Y") + "/" + "AM" if datetime.now().hour < 12 else "PM"
+        if not date:
+            date = datetime.now().strftime("%d/%m/%Y") + "/" + "AM" if datetime.now().hour < 12 else "PM"
         dimanche = datetime.now().strftime("%A") == "Sunday"
-        if len(date) == 11:
+        if len(date) == 13:
             liste = self.get_day_turnip(ctx.message.server, date)
             if liste:
                 if dimanche:
